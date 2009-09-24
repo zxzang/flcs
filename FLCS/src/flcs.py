1a85e5127e4d71e8ae4f0aecb75b95b5f59ffe10
tomasz.szycher <tomasz.szycher@gmail.com>
2009-09-24 21:18:06 +0000

    poprawa rozmycia cyka, dodanie uni do gui (nie wszystko przetestowane)

--- FLCS/src/flcs.py
+++ FLCS/src/flcs.py
@@ -11,7 +11,7 @@ from membership_Funkctions import membershipOperations
 class gramatyka():
     '''klasa zaiwera gramatyke wraz z operacja,i na niej dostepnymi'''
 
-    
+
     def __init__(self):
         #tworzenie instncji parametrow
         self.parametry = properties()
@@ -28,56 +28,56 @@ class gramatyka():
             for y in self.G[x]:
                 if len(y.right)>1:
                     self.classifierNum+=1
-                    
+
     def makeBackUp(self):
         self.G_backUp = self.G
-    
+
     def bringBackGrammar(self):
         self.G = self.G_backUp
-                    
+
     def CYK(self, slowo):
         ''' algorytm CYK '''
         print "CYK"
         #Do poprawienia:
         #1. rozszezenie algorytmu cyk o warotsc mi
         #2. back tracking
-        #3. wykorzystywanie funckji przynaleznosci 
-        
+        #3. wykorzystywanie funckji przynaleznosci
+
         # a zem wymyslil
         # 1. S-> w cos w ostatniej komorce jezeli prowadzi do poprawnego rozwiazania a nie prowadzi do nie poprawnego trza wywalic z gramatyki
-        
+
         poprawnosc = int(slowo[0])
         slowo = slowo[1:]
         #tymczasowa tablica do klasyfikatorów:
         tmp = []
         self.table_CYK=[]
-        
+
         #uzupelnianie pierwszego wiersza:
         for x in slowo:
             a = self.search(x,poprawnosc, 0)
             if a == -1:
                 if poprawnosc == 1:
-                    #rezygnuje z wprowadzania pustych znakow na pierwszym poziomie o ile zdanie jest poprawne                    
+                    #rezygnuje z wprowadzania pustych znakow na pierwszym poziomie o ile zdanie jest poprawne
                     self.pokrycieTerminalne(x)
                     a = self.search(x,poprawnosc,0)
                     tmp.append(a)
                 else:
-                    tmp.append([-1]) 
+                    tmp.append([-1])
             else:
                 tmp.append(a)
         self.table_CYK.append(tmp)
-        
+
         #zastosowanie pokrycia startowego przerwanie algorytmu
         #musi nastąpić reset ustawień w metodzie nadrzędnej
         if len(slowo) == 1:
             if self.pokrycieStartowe(slowo[0]) == 1:
                 return -10
-        
+
         #wypelnianie dalszych poziomow:
         wiersz = 1
         pozycja = 0
-                
-        while wiersz < len(slowo):  
+
+        while wiersz < len(slowo):
             tmp = []
             while pozycja < (len(slowo)-wiersz):
                 pary = self.szukajPar(wiersz,pozycja)
@@ -85,11 +85,11 @@ class gramatyka():
                 if LL == -10:
                     return -10
                 tmp.append(LL)
-                pozycja+=1                
+                pozycja+=1
             self.table_CYK.append(tmp)
             wiersz+=1
             pozycja = 0
-            
+
         #sprawdzenie poziomu sparsowania zdania:
         #tutaj także bedzie można rozmytość zastosować
         #sprawdzenie czy zdanie sie sparsowalo
@@ -100,20 +100,20 @@ class gramatyka():
                     result += 0
                 elif x[0].left == 'S':
                     result = 1
-    
-            
+
+
         #pokrycie pelne zdanie poprawne i nie sparsowane:
         if poprawnosc == 1 and result == 0 and self.allowFulCover==1:
             if self.pokryciePelne(len(slowo)-1) == 0:
                 return -10
-            
+
         #sprawdzenie poprawnosci cyka
         #if poprawnosc == result:
             #return 1
         #else:
             #return 0
         return result
-    
+
     def szukajPar(self,a,b):
         '''metoda ta zwraca pary par generujace zdanie dla odpowiedniej pozcycji, pary sa w postacji pozycji w tablicy CYK
         wiersz -> a
@@ -126,7 +126,7 @@ class gramatyka():
         for x in range(1,a):
             tablica.append([[x-1,b-1],[a-x-1,b+x-1]])
         return tablica
-    
+
     def szukajTerminal(self,K,table_CYK,poprawnosc, wiersz):
         '''metoda zwraca liste pasujacych terminali'''
         #print "szukaj terminali"
@@ -141,23 +141,25 @@ class gramatyka():
                     #do search trzeba przekazac juz odpowiednio zlozona omege:
                     #funkcja przystosowania tutaj wsadzic:
                     wynik=self.search(str(a[0].left)+str(b[0].left),poprawnosc, wiersz)
+                    #a i b tutaj sa odpowiednio ojcami nowo wyprowadzonejkomorki, dlatego najpiet liczymy dla tych wartosci unie, nastepnie miksujemy to z nowym klasyfikatorem :):
+                    union = self.membershipCounter.countFuzzyUnion(a[1], b[1])
                     if (wynik != -1) and (wynik != -10):
                         for y in wynik:
                             if y != -1:
                                 if not y in tmp:
-                                    tmp.append(y)
+                                    tmp.append((y, self.membershipCounter.countFuzzyUnion(y.omega, union)))
                     elif wynik == -10:
                         return -10
         if len(tmp)<=0:
             tmp = [-1]
         return tmp
-    
+
     def search(self,terminal,poprawnosc, poziom):
         '''metoda wyszukuje caly slownik w poszukiwaniu klasyfikatorow pasujacych do przekazanego terminalu, rozszerzona o symbol uniwersalny'''
         #print "search"
         find = 0
         result = []
-        
+
         for x in self.G:
             for T in range(0,len(self.G[x])):
                 if ((self.G[x][T].right == terminal) or (self.G[x][T].right == '#')):
@@ -165,7 +167,11 @@ class gramatyka():
                     #rezygnujemy  z przyznawania poniewaz bedziemy to robic po zakonczeniu rozbioru
                     #self.G[x][T].u[poprawnosc] += 1
                     #tutaj nalezy zastosowac jeszcze funkcje przynaleznosci i ewentualnie odrzuca niepoprawne rozwiazania z gory zakaldajac ze sa zle
-                    result.append((self.G[x][T],self.membershipCounter.countMembership()))
+                    if poziom == 0:
+                        result.append((self.G[x][T],self.G[x][T].omega))
+                    else:
+                        #tutaj trzeba funkcje uni zastosowac jak to nie jest poziom zerowy:
+                        result.append((self.G[x][T]))
                 else:
                     pass
                     #możliwość zastosowania logiki rozmytej
@@ -175,7 +181,7 @@ class gramatyka():
                         #self.pokrycieAgresywne(terminal)
 
         if find == 1:
-            return result    
+            return result
         else:
             if (self.parametry.Pa > random.random()) and (poprawnosc == 1) and (poziom > 1):
                 if self.pokrycieAgresywne(terminal) == 1:
@@ -183,22 +189,22 @@ class gramatyka():
             else:
                 print "no i nie weszlo w pokrycie agresywne"
         return -1
-        
+
     def pokrycieTerminalne(self, term):
         '''operacja pokrycia terminalnego, przyjmuje terminal do dodania'''
         print "pokrycie terminalne"
         return self.add(string.upper(term) , term)
-    
+
     def pokrycieStartowe(self, term):
         '''pokrycie startowe'''
         print "pokrycie startowe"
         return self.add('S',term)
-    
+
     def pokryciePelne(self, poziom):
         '''FullCover zwraca -1 gdy pokrycie nie jest mozliwe, lub 0 gdy doda terminal taki jak trza'''
         print "pokrycie pelne"
         tab_par=self.szukajPar(poziom,0)
-        
+
         for x in tab_par:
             for a in self.table_CYK[x[0][0]][x[0][1]]:
                 if a == -1:
@@ -209,9 +215,9 @@ class gramatyka():
                     self.add('S',str(a[0].left)+str(b[0].left))
                     self.allowFulCover = 0
                     return 0
-                    
+
         return -1
-        
+
     def pokrycieAgresywne(self, terminal):
         '''i tego tez nie rozumiem
         zorbie tak: ze zbioru detektorow wybieram 3 i robie z nich klasyfikator'''
@@ -221,19 +227,19 @@ class gramatyka():
         D = self.G.keys()
         tmp = random.randint(0,len(D)-1)
         return self.add(D[tmp],terminal)
-    
+
     def getGramarStr(self):
         '''metoda zwraca liste klasyfikatorow wraz z ich parametrami w postaci stringa'''
         tmp = ""
         for x in self.G:
             for y in self.G[x]:
                 tmp+=y.getClassifierStrExt()
-        return tmp   
-    
+        return tmp
+
     def getDictionaryStr(self):
         pass
-    
-    
+
+
     def add(self, A, B):
         '''dodaje kolejne reguly do gramamtyki A-->B'''
         print "add"
@@ -246,7 +252,7 @@ class gramatyka():
                     #znalazlem juz taki klasyfikator:
                     tmp=1
                     return -1
-                
+
             if tmp == 0:
                 #scisk tutaj:
                 if(self.classifierNum >= self.parametry.np):
@@ -257,10 +263,10 @@ class gramatyka():
                     self.classifierNum+=1
                 print "dodalem"
                 print str(A) +'-->' +str(B)
-            
+
                 return 1
-            
-            else:                
+
+            else:
                 #print "nie dodalem bo juz jest:)"
                 return -1
         else:
@@ -271,19 +277,19 @@ class gramatyka():
             print str(A) +'-->' +str(B)
             self.classifierNum+=1
             return 1
-        
+
     def rem(self,class_rem):
         '''metoda usowa klasyfikator z gramatyki'''
         print "rem"
         i = 0
-        for x in self.G[class_rem.left]: 
+        for x in self.G[class_rem.left]:
             if x.compare(class_rem) == len(class_rem.right)+1:
-               
+
                 print 'usuwam'
                 print self.G[class_rem.left][i]
                 self.classifierNum-=1
                 del self.G[class_rem.left][i]
-                
+
                 if len(self.G[class_rem.left])==0:
                     del(self.G[class_rem.left])
                 print self.classifierNum
@@ -291,7 +297,7 @@ class gramatyka():
             i += 1
         print "zem nie usunal !!!!!!!!!!!!!!!!!!!!"
         return 0
-    
+
     def scisk(self, left, name):
         '''metoda dodaje dany klasyfikator ze sciskiem left-->name'''
         print "scisk"
@@ -299,23 +305,23 @@ class gramatyka():
         cf_table = []
         choose = 0
         minmal = 100000
-        
+
         for y in range(0,self.parametry.cf):
             for x in range(0,self.parametry.cs):
                 keys  = self.G.keys()
-               
+
                 while 1:
                     B = random.choice(keys)
                     #if len(self.G[B])>1:
                         #break
                     K = self.G[B][random.randint(0,len(self.G[B])-1)]
                     if len(K.getRight()) > 1:
-                        break                    
-                    
+                        break
+
                 if K.fitness <=  minmal:
                     minmal = K.fitness
                     choose = K
-                    
+
             cf_table.append(K)
 
         #return cf_table
@@ -328,17 +334,17 @@ class gramatyka():
             if podobienstwo_max < podobienstwo:
                 choose = x
 
-        
+
         #usuwamy najbardziej podobny z klasyfikatorów:
         #self.rem(choose.left, choose.right)
         #dodaje nowy klasyfikator:
         #self.add(left,name)
         return self.rem(choose)
-    
+
     def countProfit(self, rozbior, word_len, poprawnosc):
         '''metoda liczy profit uzyskany odczas rozbioru poprawnego/niepoprawnego'''
         print "count profit"
-        
+
         #sprawdzamy czy rzobior byl napewno do konca zrobiony:
         result = 0
         for y in self.table_CYK[word_len]:
@@ -347,7 +353,7 @@ class gramatyka():
                     pass
                 elif x[0].left == 'S':
                     result = 1
-        
+
         for x in range(0,word_len):
             for y in self.table_CYK[x]:
                 for n in y:
@@ -359,19 +365,19 @@ class gramatyka():
                             else:
                                 n[0].d += self.parametry.ba*pow(2,x)*self.parametry.raf
         #if rozbior == 0:
-            #for x in self.usedTerminals:            
+            #for x in self.usedTerminals:
                 #x[0].d += self.parametry.ba*pow(2,x[1])*self.parametry.raf
         #else:
-            #for x in self.usedTerminals:            
+            #for x in self.usedTerminals:
                 #x[0].p += self.parametry.ba*pow(2,x[1])*self.parametry.ra
-   
+
     def countFitness(self):
         #przeniesc to do implementacji klasyfikatora!!!
         print "count fitness"
         #Pd jest (maximum,minimum)
         PD = self.getPD()
         fc = ff = 0
-        
+
         for x in self.G:
             for y in self.G[x]:
                 #liczenie ff:
@@ -379,14 +385,14 @@ class gramatyka():
                     ff = (y.p -y.d - PD[1])/(PD[0]-PD[1])
                 else:
                     ff = 0.0
-                
+
                 #liczenie fc:
                 if((y.u[0]+y.u[1])==0):
                     fc = self.parametry.f0
                 else:
                     if (self.parametry.wn*y.u[0]+self.parametry.wp*y.u[1]) != 0:
                         fc = (self.parametry.wp * y.u[1])/(self.parametry.wn*y.u[0]+self.parametry.wp*y.u[1])
-                
+
                 #liczenie przystosowania:
                 if (self.parametry.wc + self.parametry.wf) != 0:
                     y.fitness = (self.parametry.wc*fc + self.parametry.wf*ff)/(self.parametry.wc + self.parametry.wf)
@@ -404,14 +410,14 @@ class gramatyka():
                 if tmp > maximum:
                     maximum = tmp
         return (maximum,minimum)
-    
+
     def setParams(self,properties):
         self.parametry = properties
 
-    def getParams(self,properties):
-        self.parametry = properties
-        
-    
+    def getParams(self):
+        return self.parametry
+
+
 #testy:
 G = gramatyka()
 gen = genetyk(G,G.parametry)
@@ -427,11 +433,11 @@ for x in tab:
 G.countProfit(result,len('1abba')-2,1)
 G.countFitness()
 
-print G.getGramarStr()    
+print G.getGramarStr()
 
 print "genetyczny przedsawiamy:"
 gen.makeGen()
-#tmp.right = "dupa" 
+#tmp.right = "dupa"
 print G.getGramarStr()
 
 
--- FLCS/src/membership_Funkctions.py
+++ FLCS/src/membership_Funkctions.py
@@ -7,77 +7,106 @@ class membershipOperations():
     def __init__(self, parent):
         self.parent = parent
         self.functionType = "random"
-        pass
-    
+
     def fullRandom(self):
         ''' totalny random funkcji przynaleznosci'''
         return random.random()
-    
+
     def zadehUnion(self, a, b):
         return max(a,b)
-    
+
     def zadehConjunction(self, a, b):
         return min(a,b)
-    
+
     def yagerUnion(self, a, b):
         wu = self.parent.getParams().yagerWu
         tmp = (a**wu +b**wu)**(1/wu)
         return min(1.0,tmp)
-    
+
     def yagerConjunction(self, a, b):
         wi = self.parent.getParams().yagerWi
         return 1.0 - min(((1.0-a)**wi + (1.0-b)**wi)**(1.0/wi))
-    
+
     def duboisUnion(self, a, b):
         alfaI = self.parent.getParams().duboisAlfai
         alfaU = self.parent.getParams().duboisAlfau
         return (a+b - a*b - min(a,b,1.0-alfaU))/max(1.0-a,1.0-b,alfaU)
-    
+
     def duboisConjunction(self, a, b):
         alfaI = self.parent.getParams().duboisAlfai
         alfaU = self.parent.getParams().duboisAlfau
         return (a*b)/max(a,b,alfaI)
-    
+
     def hamacherUnion(self, a, b):
         qu = self.parent.getParams().hamacherQu
         return (a+b-(2.0 - qu)*a*b)/(1-(1-qu)*a*b)
-    
+
     def hamacherConjunction(self, a, b):
         qi = self.parent.getParams().hamacherQi
         return (a*b)/(qi + (1.0 - qi)(a+b+a*b))
-    
+
     def dombiUnion(self, a, b):
         wu = self.parent.getParams().dombiWu
         return 1.0/((1.0+((1/a)-1)**(-1.0*wu) + ((1.0/a)-1)**(-1.0*wu)))**(-1.0/wi)
-        
+
     def dombiConjunction(self, a, b):
         wi = self.parent.getParams().dombiWi
         return 1.0/((1.0+((1/a)-1)**(-1.0*wi) + ((1.0/a)-1)**(-1.0*wi)))**(-1.0/wi)
-    
-    
-    def countMembership(self):
-        '''tutaj bedzie juz liczone to co tzeba, nie wiem jeszcze co dokaldnie tutaj bedize potrzbne do przekazania'''
+
+
+    def countFuzzyUnion(self, a, b):
+        '''tutaj bedzie juz liczone unia w zaleznosci od wybranej metody'''
+        self.functionType = self.parent.getParams().fuzzyUnion
+
+        if self.functionType == "random" or self.functionType == "none":
+            return self.fullRandom()
+        elif self.functionType == "Yager":
+            return self.yagerUnion(a, b)
+        elif self.functionType == "Zedeh":
+            return self.zedehUnion(a, b)
+        elif self.functionType == "Dubois":
+            return self.duboisUnion(a,b)
+        elif self.functionType == "Hamcher":
+            return self.hamacherUnion(a,b)
+        elif self.functionType == "Dombi":
+            return self.dombirUnion(a,b)
+        else:
+            return -99
+
+    def countFuzzyConjunction(self, a, b):
+        '''tutaj bedzie juz liczone unia w zaleznosci od wybranej metody'''
         if self.functionType == "random":
             return self.fullRandom()
+        elif self.functionType == "Yager":
+            return self.yagerConjunction(a, b)
+        elif self.functionType == "Zedeh":
+            return self.zedehConjunction(a, b)
+        elif self.functionType == "Dubois":
+            return self.duboisConjunction(a,b)
+        elif self.function == "Hamcher":
+            return self.hamacherConjunction(a,b)
+        elif self.function == "Dombi":
+            return self.dombirConjunction(a,b)
         else:
-            return "kupa ;)"
-    
+            return -99
+
+
     def setFunction(self,function):
         self.functionType = function
-        
- 
+
+
 class membershipFunctions():
     ''' klasa musi z parenta zczytac parametry przetwarzania i uruchomic odpowiednia funkcje przynaleznosci po kazdym zakonczonym rozkladzie zestawu uczacego'''
     def __init__(self, parent):
         self.parent = parent
         self.getParams()
-        
+
     def getParams(self):
         '''metoda pobiera parametry od rodzica i odpowiednio je ustwaia'''
         parametry = self.parent.getParams()
         self.memberShipType = str(parametry.memebershipFunction)
         self.generalization = str(parametry.generalization)
-    
+
     def typeS(self):
         '''funkcja ma za zadanie obliczenie funkcji przynaleznosci dla calej gramatyki'''
         a=b=c=0.0
@@ -96,18 +125,18 @@ class membershipFunctions():
                     y.omega= 1-2*((n-a)/(c-a))
                 else:
                     y.omega = 1.0
-                    
+
     def kwadrat(self):
         gramatyka = self.parent.getGrammar()
         for x in gramatyka.G:
             for y in gramatyka.G[x]:
                 y.omega = y.omega*y.omega
-        
+
     def pierwiastek(self):
         for x in gramatyka.G:
             for y in gramatyka.G[x]:
                 y.omega = y.omega**0.5
-        
+
     def countMembership(self):
         self.getParams()
         print self.memberShipType
@@ -122,11 +151,9 @@ class membershipFunctions():
             self.pierwiastek()
         else:
             print "brak generalizacji"
-    
-    
-cos = membershipOperations("assa")
-print cos.countMembership()
-        
-    
-    
-    
+
+
+
+
+
+
